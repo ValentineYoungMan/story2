0543a53658ff99af1b31dc73e39fb9dfbafe7776
ValentineYoungMan <90317015+ValentineYoungMan@users.noreply.github.com>
2022-05-19 20:37:27 +0300

    Create app-story.js

---
+++ js/app-story.js
@@ -0,0 +1,140 @@
+function ibg() {
+
+    let ibg = document.querySelectorAll(".ibg");
+    for (var i = 0; i < ibg.length; i++) {
+        if (ibg[i].querySelector('img')) {
+            ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+        }
+    }
+}
+
+ibg();
+
+//----------------------------------------------
+
+//----------------------------------------------
+
+
+//menu burger
+const iconMenu = document.querySelector('.menu-icon');
+const menuBody = document.querySelector('.menu-body');
+
+let unlock = true;
+
+
+if (iconMenu) {
+    
+    iconMenu.addEventListener("click", function(e) {
+        // document.body.classList.toggle('_lock')
+        // iconMenu.classList.toggle('_active');
+        // menuBody.classList.toggle('_active');
+        
+
+        if (unlock===true && !iconMenu.classList.contains('_active')){                         //menuBody.classList.contains('_active') && 
+            menuBody.classList.add('top-active');
+            document.body.classList.add('_lock')
+            iconMenu.classList.add('_active');
+            menuBody.classList.add('_active');
+            animStart()
+            unlock=false;
+        } else if(unlock===false && iconMenu.classList.contains('_active')) {
+            document.body.classList.remove('_lock')
+            iconMenu.classList.remove('_active');
+            menuBody.classList.remove('_active');
+            animEnd()
+            setTimeout(()=>{
+            
+                unlock=true;
+                menuBody.classList.remove('top-active');
+                
+            }, 1200);
+            
+        }
+
+    });
+}
+
+//------------------------------
+
+
+
+const animItems = document.querySelectorAll('._anim-items');
+
+function animStart() {
+    console.log('fewgbfs')
+    if (animItems.length > 0) {
+        for (let i = 0; i < animItems.length; i++) {
+            let animItem = animItems[i];
+            
+            animItem.classList.add('_active2')
+        }
+    }
+}
+
+function animEnd() {
+    console.log('p')
+    if (animItems.length > 0) {
+        for (let i = 0; i < animItems.length; i++) {
+            let animItem = animItems[i];
+            animItem.classList.remove('_active2')
+        }
+    }
+}
+
+//---------------------------------------------------------
+
+const viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+window.addEventListener("DOMContentLoaded", () => {
+    if (animItems.length > 0) {
+        for (let i = 0; i < animItems.length; i++) {
+            let animItem = animItems[i];
+            
+            if (viewport_width > 767) {
+                animItem.classList.add('_anim-initial')
+            } else{
+                animItem.classList.remove('_anim-initial')
+            }
+        }
+    }
+})
+
+//------------------------------
+
+let header = document.querySelector('.header');
+let block2 = document.querySelector('.block2');
+let body = document.querySelector('.body');
+
+let scrollY1 = 0;
+
+window.addEventListener('scroll', ()=>{
+
+    let scrollY2 = window.pageYOffset;
+
+     if(document.documentElement.scrollTop===0) {
+            
+        setTimeout(()=>{
+            header.classList.remove('blackBackground')
+        }, 300);
+
+        scrollY1 = scrollY2    
+    } else if (scrollY2 < scrollY1){
+        header.classList.add('blackBackground')
+        header.classList.remove('hide')
+        scrollY1 = scrollY2 
+    }else if(document.documentElement.scrollTop!==0){
+        header.classList.add('hide')
+        scrollY1 = scrollY2
+    }
+})
+
+
+//------------------------------
+
+
+
+//------------------------------
+
+
+
+
